3045ed2408282e24272135a49afac8a4f8a73929
ehwaman <ehwaman@gmail.com>
2024-06-12 14:22:49 +0900

    초기 커밋.

---
+++ Baseball123/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Baseball123
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textNumber = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.새로시작하기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuNewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "서로 다른 3자리 숫자를 입력하세요.";
+            // 
+            // textNumber
+            // 
+            this.textNumber.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textNumber.Location = new System.Drawing.Point(256, 47);
+            this.textNumber.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textNumber.MaxLength = 3;
+            this.textNumber.Name = "textNumber";
+            this.textNumber.Size = new System.Drawing.Size(45, 23);
+            this.textNumber.TabIndex = 1;
+            this.textNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textNumber_KeyPress);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(305, 47);
+            this.btnGo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(60, 25);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 12;
+            this.listBox.Items.AddRange(new object[] {
+            "[새로 시작하기]를 해 주세요."});
+            this.listBox.Location = new System.Drawing.Point(55, 103);
+            this.listBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(198, 244);
+            this.listBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(283, 103);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Go 횟수 :";
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(348, 103);
+            this.labelCount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(11, 12);
+            this.labelCount.TabIndex = 5;
+            this.labelCount.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.새로시작하기ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(412, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 새로시작하기ToolStripMenuItem
+            // 
+            this.새로시작하기ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuNewGame,
+            this.MenuExit});
+            this.새로시작하기ToolStripMenuItem.Name = "새로시작하기ToolStripMenuItem";
+            this.새로시작하기ToolStripMenuItem.Size = new System.Drawing.Size(50, 22);
+            this.새로시작하기ToolStripMenuItem.Text = "Menu";
+            // 
+            // MenuNewGame
+            // 
+            this.MenuNewGame.Name = "MenuNewGame";
+            this.MenuNewGame.Size = new System.Drawing.Size(180, 22);
+            this.MenuNewGame.Text = "새로 시작하기";
+            this.MenuNewGame.Click += new System.EventHandler(this.MenuNewGame_Click);
+            // 
+            // MenuExit
+            // 
+            this.MenuExit.Name = "MenuExit";
+            this.MenuExit.Size = new System.Drawing.Size(180, 22);
+            this.MenuExit.Text = "종료하기";
+            this.MenuExit.Click += new System.EventHandler(this.MenuExit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(53, 63);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "(1~9)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 388);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.textNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "숫자 야구 게임";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textNumber;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 새로시작하기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuNewGame;
+        private System.Windows.Forms.ToolStripMenuItem MenuExit;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Baseball123/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Baseball123
+{
+    public partial class Form1 : Form
+    {
+        private string ResultNumber;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuNewGame_Click(object sender, EventArgs e)
+        {
+            int value1 = 0, value2 = 0, value3 = 0;
+
+            //난수 생성을 위한 클래스 선언
+            Random random = new Random();
+
+            //서로 다른 3자리 숫자를 만들기 위해 계속 반복 작업 수행
+            //무한루트를 방지하기 위해 1000번까지만 반복
+            for (int i = 0; i < 1000; i++)
+            {
+                int rnd = random.Next(1, 10);   //1~9 사이의 단일 정수
+
+                if (value1 == 0)
+                    value1 = rnd;
+                else if (value1 != 0 && value2 == 0 && value1 != rnd)
+                    value2 = rnd;
+                else if (value1 != 0 && value2 != 0 && value3 == 0 && value1 != rnd && value2 != rnd)
+                    value3 = rnd;
+
+                if (value3 != 0)
+                    break;
+            }
+
+            //난수 생성으로 만들어진 서로 다른 3자리 숫자를 합침 = 찾아야 할 3자리 숫자
+            if (value1 != 0 && value2 != 0 && value3 != 0)
+                ResultNumber = value1.ToString() + value2.ToString() + value3.ToString();
+
+            //리스트박스, 실행 횟수 클리어
+            listBox.Items.Clear();
+            labelCount.Text = "0";
+
+        }
+
+        private void MenuExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            NumberCheck();
+        }
+
+        private void NumberCheck()
+        {
+            if (ResultNumber == null || ResultNumber == "")
+            {
+                MessageBox.Show("Menu에서 [새로 시작하기]를 해 주세요.");
+                return;
+            }
+
+            if (textNumber.Text.Length != 3)
+            {
+                MessageBox.Show("서로 다른 3자리 숫자를 입력하세요.");
+                return;
+            }
+
+            if (textNumber.Text.Substring(0, 1) == textNumber.Text.Substring(1, 1) ||
+                textNumber.Text.Substring(0, 1) == textNumber.Text.Substring(2, 1) ||
+                textNumber.Text.Substring(1, 1) == textNumber.Text.Substring(2, 1))
+            {
+                MessageBox.Show("서로 다른 3자리 숫자를 입력하세요.");
+                return;
+            }
+
+            //시도 횟수를 1씩 누적시킴
+            labelCount.Text = (int.Parse(labelCount.Text) + 1).ToString();
+
+            int Strike = 0, Ball = 0;
+
+            //위치하는 자리수가 일치하면 Strike 판정
+            if (ResultNumber.Substring(0, 1) == textNumber.Text.Substring(0, 1))
+                Strike += 1;
+            if (ResultNumber.Substring(1, 1) == textNumber.Text.Substring(1, 1))
+                Strike += 1;
+            if (ResultNumber.Substring(2, 1) == textNumber.Text.Substring(2, 1))
+                Strike += 1;
+
+            //3자리 숫자에 포함은 되지만 위치하는 자리수가 다른 경우 Ball 판정
+            if (ResultNumber.Substring(0, 1) == textNumber.Text.Substring(1, 1) || ResultNumber.Substring(0, 1) == textNumber.Text.Substring(2, 1))
+                Ball += 1;
+            if (ResultNumber.Substring(1, 1) == textNumber.Text.Substring(0, 1) || ResultNumber.Substring(1, 1) == textNumber.Text.Substring(2, 1))
+                Ball += 1;
+            if (ResultNumber.Substring(2, 1) == textNumber.Text.Substring(0, 1) || ResultNumber.Substring(2, 1) == textNumber.Text.Substring(1, 1))
+                Ball += 1;
+
+            //리스트박스에 판정결과 출력
+            string ResultMsg = string.Format("{0} Strike {1} Ball", Strike.ToString(), Ball.ToString());
+
+            //마지막 입력한 내용이 첫째줄에 오도록 Insert() 인덱스 0에 추가함
+            //끝줄에 출력되도록 하려면 listBox.Items.Add()로 해주면 됨
+            listBox.Items.Insert(0, string.Format("[{0}] {1} - {2}", labelCount.Text, textNumber.Text, ResultMsg));
+
+            if (Strike == 3)
+            {
+                MessageBox.Show("숫자를 모두 맞추었습니다.");
+                //현재 게임을 종료함
+                ResultNumber = "";
+            }
+
+            textNumber.Text = "";
+        }
+
+        private void textNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //숫자를 입력하고 엔터를 쳤을 때도 판정 실행
+            if (e.KeyChar == 13)
+            {
+                NumberCheck();
+            }
+        }
+
+    }
+}
